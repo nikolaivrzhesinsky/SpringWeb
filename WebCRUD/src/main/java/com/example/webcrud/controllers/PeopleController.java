fe6809896f34be85e261bba9c258d55ff6092632
Николай Зотин <15nk33l49@gmail.com>
2022-03-17 01:54:28 +0300

    add, read

---
+++ WebCRUD/src/main/java/com/example/webcrud/controllers/PeopleController.java
@@ -0,0 +1,53 @@
+package com.example.webcrud.controllers;
+
+
+import com.example.webcrud.dao.PersonDao;
+import com.example.webcrud.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PeopleController(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+
+    @GetMapping()
+    public String index(Model model){
+        model.addAttribute("people",personDao.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model){
+        model.addAttribute("person",personDao.show(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model){
+        model.addAttribute("person",new Person());
+        return "people/new";
+    }
+
+    @PostMapping()
+    public  String create(@ModelAttribute("person") Person person){
+
+        personDao.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model,@PathVariable("id") int id){
+        model.addAttribute("person",personDao.show(id));
+        return "people/edit";
+    }
+}
