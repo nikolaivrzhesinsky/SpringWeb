cf159e17b223c58025e920fbefa24a54a615b772
Николай Зотин <15nk33l49@gmail.com>
2022-03-17 21:59:42 +0300

    PharmacyWeb

---
+++ PharmacyLoyalty/src/main/java/com/example/pharmacyloyalty/PharmacyLoyaltyApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacyloyalty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyLoyaltyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyLoyaltyApplication.class, args);
+    }
+
+}
